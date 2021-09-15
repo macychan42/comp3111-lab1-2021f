6e7c2b4d9602f42d957d5834f091ad6ac1412513
macyc <macyc@LAPTOP-EUL0MCEI.Fujitsu.login>
2021-09-15 22:42:46 +0800

    I have made a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -6,7 +6,9 @@ package Lab1;
 public class Library {
 	
 	public static void main(String arg[]) {
-		System.out.println("Hello, Wordl!");
+		Library lib = new Library();
+		if (lib.someLibraryMethod())
+			System.out.println("Hello, Wordl!");
 	}
     public boolean someLibraryMethod() {
         return true;
